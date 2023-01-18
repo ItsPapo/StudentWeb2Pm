da52eaa48ba0d4b50a3e18a13f6d0df46e9e7f58
ItsPapo <cacarlos@sheridancollege.ca>
2023-01-18 16:30:46 -0500

    This Commit has 5 student names

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -17,8 +17,21 @@ public class StudentList {
     {
        Student [] studentList = new Student[1];//change size before running
        Student s1 = new Student();
+       Student s2 = new Student();
+       Student s3 = new Student();
+       Student s4 = new Student();
+       Student s5 = new Student();
        s1.setName("Carlos Castro");
+       s1.setName("Carlos Castro 2");
+       s1.setName("Carlos Castro 3");
+       s1.setName("Carlos Castro 4");       
+       s1.setName("Carlos Castro 5");
+                            
        studentList[0]=s1;
+       studentList[1]=s2;
+       studentList[2]=s3;
+       studentList[3]=s4;
+       studentList[4]=s5;
        //fill in 4 student names before running
        for(Student s: studentList)
        {
